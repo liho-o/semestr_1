bc5fa54d5dcf36f2d180c04d0a95bd5ebb50a7ee
liho-o <67860443+liho-o@users.noreply.github.com>
2024-02-11 20:04:39 +0700

    10

---
+++ 10.py
@@ -0,0 +1,9 @@
+flynumber = input("номер рейса: ")
+company_name_ru = input("название авиакомпании (на русском языке):")
+company_name_eng = input("название авиакомпании (на английском языке): ")
+end_city_name_ru = input("город прилета (на русском языке):")
+end_city_name_eng = input("город прилета (на английском языке):")
+print(f'Заканчивается посадка на рейс {flynumber} авиакомпании {company_name_ru} до {end_city_name_ru}')
+print(f'This is the final boarding call for {company_name_eng} flight {flynumber} to {end_city_name_eng} ')
+
+
